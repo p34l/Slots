3d04b92c64b0536d79c75a54ab0e847b12822b68
p34l <utyg24@gmail.com>
2025-10-03 18:53:24 +0300

    Initial Commit

---
+++ Slots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slots
+//
+//  Created by Misha Kandaurov on 03.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
